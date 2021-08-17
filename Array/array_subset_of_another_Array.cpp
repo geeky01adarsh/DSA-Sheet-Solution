fbe60e6f3fe413409885801e140d38361405614f
Adarsh Navneet Sinha <adarsh91094@gmail.com>
2021-08-17 18:43:17 +0530

    array subset of another array

---
+++ Array/array_subset_of_another_Array.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string isSubset(int a1[], int a2[], int n, int m) ;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        int a1[n], a2[m];
+
+        for (int i = 0; i < n; i++) {
+            cin >> a1[i];
+        }
+        for (int i = 0; i < m; i++) {
+            cin >> a2[i];
+        }
+
+        cout << isSubset(a1, a2, n, m) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+string isSubset(int a1[], int a2[], int n, int m) {
+    int arr[100001]={0};
+    for(int i=0; i<n; i++){
+        arr[a1[i]]++;
+    }
+    for(int j=0; j<m; j++){
+        if(arr[a2[j]]--) continue;
+        else return "No";
+    }
+    return "Yes";
+}
