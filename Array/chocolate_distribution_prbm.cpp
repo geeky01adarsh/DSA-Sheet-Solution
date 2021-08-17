db11b5a65acc9e3c6acb246a4d2dd2f112e3f2bb
Adarsh Navneet Sinha <adarsh91094@gmail.com>
2021-08-18 01:47:29 +0530

    chocolate distribution prbm

---
+++ Array/chocolate_distribution_prbm.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+    //code
+        sort(a.begin(),a.end());
+        // for(auto i:a) cout<<i<<" ";
+        long long ans =INT_MAX;
+        for(int i=0;i<=(n-m); i++){
+            ans = min(ans, a[i+m-1]-a[i]);
+        }
+        return ans;
+    }   
+};
+
+// { Driver Code Starts.
+int main() {
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		vector<long long> a;
+		long long x;
+		for(long long i=0;i<n;i++)
+		{
+			cin>>x;
+			a.push_back(x);
+		}
+		
+		long long m;
+		cin>>m;
+		Solution ob;
+		cout<<ob.findMinDiff(a,n,m)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
